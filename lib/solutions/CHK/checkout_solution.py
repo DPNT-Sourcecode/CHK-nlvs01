0da23a3c01efb2f314cd4fd4dddd81dd5a84e8af
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 19:16:03 +0100

    Sat Aug 11 19:16:03 BST 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -70,6 +70,8 @@ def any_of_three(skus, bill):
                 to_pop = []
                 last_item = item
                 continue
+        if tot == 0:
+            break
     if last_item is not None:
         bill[last_item]['offers'].append({'items': tot, 'price': 45})
     return skus, bill
