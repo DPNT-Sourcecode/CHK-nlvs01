49ab33914609dc3bc25bc3b124748d6f0070cf37
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 13:11:24 +0100

    Sat Aug 11 13:11:24 BST 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,7 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    raise NotImplementedError()
+    if isinstance(friend_name, str):
+        return "Hello {}".format(friend_name)
+    else:
+        raise ValueError('Please provide a string')
