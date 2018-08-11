f4cf1f21f0e1f7295427adb286a6f2b0227e3d5a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 13:12:18 +0100

    Sat Aug 11 13:12:18 BST 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,4 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    if isinstance(friend_name, str):
-        return "Hello {}".format(friend_name)
-    else:
-        raise ValueError('Please provide a string')
+    return 'Hello World!'
