e65787079bcab51dad3c613129e363f012f6b2a1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 13:00:16 +0100

    Sat Aug 11 13:00:16 BST 2018

--- lib/solutions/sum_r1.py
+++ lib/solutions/sum_r1.py
@@ -3,8 +3,8 @@ def sum(x, y):
 
     """
     !!Function name is shadowing built in sum!!
-    :param x: an integer number
-    :param y: an integer number
+    :param x: an integer number between 0 and 100
+    :param y: an integer number between 0 and 100
     :return: sum of x + y
     """
     if all(isinstance(n, int) for n in (x, y)) and all(0 <= n <= 100 for n in (x, y)):
