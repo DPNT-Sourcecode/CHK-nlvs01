c7953dc6d5566d9b601b8cf96511232113e0a2a4
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 13:04:26 +0100

    Sat Aug 11 13:04:26 BST 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,12 @@
-# noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def sum(x, y):
+
+    """
+    !!Function name is shadowing built in sum!!
+    :param x: an integer number between 0 and 100
+    :param y: an integer number between 0 and 100
+    :return: sum of x + y
+    """
+    if all(isinstance(n, int) for n in (x, y)) and all(0 <= n <= 100 for n in (x, y)):
+        return x + y
+    else:
+        raise ValueError('Please provide two integer numbers')
