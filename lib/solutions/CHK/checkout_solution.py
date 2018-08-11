f259b904d825f9ca128add9b72d074cc274ca88a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 15:40:44 +0100

    Sat Aug 11 15:40:44 BST 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,20 +2,38 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+def get_free_items(skus_items, offer_type, offer_quantity, free_item):
+    """
+    :param skus_item: skus items list
+    :param offer_type: the type of item that defines the offer
+    :param offer_quantity: the quanity of the item that defines the offer
+    :param free_item: the item to remove from skus_items
+    :return: new skus_items list without the items on offer
+    """
+    to_remove = offer_quantity / 2
+    for i in range(to_remove):
+        if free_item in skus_items:
+            skus_items.pop(skus_items.index(free_item))
+    return skus_items
+
+
 def checkout(skus):
     skus = [c for c in skus]
+    # items to remove
     for s in set(skus):
         quantity = skus.count(s)
         if s == 'E' and quantity / 2 > 0 and 'B' in skus:
-            to_remove = quantity / 2
-            for i in range(to_remove):
-                if 'B' in skus:
-                    skus.pop(skus.index('B'))
+            skus = get_free_items(skus, 'E', 2, 'B')
         if s == 'F' and quantity / 3 > 0:
-            to_remove = quantity / 3
-            for i in range(to_remove):
-                if 'F' in skus:
-                    skus.pop(skus.index('F'))
+            skus = get_free_items(skus, 'F', 3, 'F')
+        if s == 'N' and quantity / 3 > 0 and 'M' in skus:
+            skus = get_free_items(skus, 'N', 3, 'M')
+        if s == 'R' and quantity / 3 > 0 and 'Q' in skus:
+            skus = get_free_items(skus, 'R', 3, 'Q')
+        if s == 'U' and quantity / 3 > 0:
+            skus = get_free_items(skus, 'U', 3, 'U')
+
     prices = []
     for s in set(skus):
         quantity = skus.count(s)
@@ -37,13 +55,19 @@ def checkout(skus):
                 quantity = quantity % 2
             prices.append(sum([offer_two * 45, quantity * 30]))
         elif s == 'C':
-            prices.append(quantity * 20)
+            unit_price = 20
+            tot_price = unit_price
         elif s == 'D':
-            prices.append(quantity * 15)
+            unit_price = 15
         elif s == 'E':
-            prices.append(quantity * 40)
+            unit_price = 40
         elif s == 'F':
-            prices.append(quantity * 10)
+            unit_price = 10
+        elif s == 'G':
+            unit_price = 20
+        elif s == 'F':
+            unit_price = 10
+
         else:
             return -1
     return sum(prices)
