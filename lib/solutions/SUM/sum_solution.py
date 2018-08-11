edf4cb5a3b02bd87ff37ef36b117b09115ffa793
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 13:07:52 +0100

    Sat Aug 11 13:07:52 BST 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+    if all(0 <= n <= 100 for n in (x, y)):
+        return x + y
+    else:
+        raise ValueError('Please provide two integer numbers')
