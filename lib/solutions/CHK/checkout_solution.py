9f29dbe1c2a8962091650aa9c1713d8f94f11d88
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 13:32:42 +0100

    Sat Aug 11 13:32:42 BST 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,5 +3,42 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skuslooku
-    raise NotImplementedError()
+    skus_lookup = {
+        'A': {
+            'price': 50,
+            'offer': {
+                'items': 3,
+                'price': 130
+            }
+        },
+        'B': {
+            'price': 50,
+            'offer': {
+                'items': 2,
+                'price': 45
+            }
+        },
+        'C': {
+            'price': 20
+        },
+        'D': {
+            'price': 15
+        },
+
+    }
+    prices = []
+    for s in set(skus):
+        quantity = skus.count(s)
+        item = skus_lookup[s]
+        item_price = 0
+        if 'offer' in item:
+            if quantity >= item['offer']['items']:
+                offer_items = quantity / item['offer']['items']
+                normal_items = quantity % item['offer']['items']
+                offer_price = offer_items * item['offer']['price']
+                normal_price = normal_items * item['price']
+                item_price = sum([normal_price, offer_price])
+        else:
+            item_price = item['price'] * quantity
+        prices.append(item_price)
+    return sum(prices)
