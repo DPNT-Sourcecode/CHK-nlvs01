a41ee408f1821987373d62a5ae7288d5a69ebb41
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-08-11 14:50:17 +0100

    Sat Aug 11 14:50:17 BST 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    skus = [c for c in skus]
     for s in set(skus):
-        skus_list =
         quantity = skus.count(s)
         if s == 'E' and quantity % 2 == 0 and 'B' in skus:
             to_remove = quantity / 2
             for i in range(to_remove):
                 if 'B' in skus:
-                    skus = skus.replace('B', '')
+                    skus.pop(skus.index('B'))
     prices = []
     for s in set(skus):
         quantity = skus.count(s)
